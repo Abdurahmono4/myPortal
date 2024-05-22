8fe61876678f54f850caa6f46205cf9c61940373
Abdurahmono4 <olimjonovrahmonjon7@gmail.com>
2024-05-23 03:06:07 +0500

    myPortal

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let pos = document.documentElement;
+
+pos.addEventListener("mousemove", (e) => {
+  pos.style.setProperty("--x", e.clientX + "px");
+  pos.style.setProperty("--y", e.clientY + "px");
+
+  console.log("mousemove");
+});
